1bb669c1cafd21f41455c89985e928b1f146e0e1
ZappaBoy <federico.zappone@justanother.cloud>
2023-04-26 02:14:42 +0200

    Added variables expansion;

--- src/models/config_model.py
+++ src/models/config_model.py
@@ -1,15 +1,31 @@
+import re
 import tomllib
+
+expandable_variables_pattern = re.compile(r"\$\{.*}")
 
 
 class Config:
+    variables: dict = {}
 
     def __init__(self, config_path: str):
         self.config_path = config_path
-        print(f'Config loaded from {config_path}')
         config_file_content = self.read_config_file(config_path)
-        print(f'Config file content: {config_file_content}')
+        self.variables = config_file_content['variables']
+        self.variables = self.expand_variables(self.variables)
+        print(self.variables)
 
     @staticmethod
     def read_config_file(config_path: str):
         with open(config_path, 'rb') as config_file:
             return tomllib.load(config_file)
+
+    @staticmethod
+    def expand_variables(variables: dict):
+        for key, value in variables.items():
+            variables_to_expand = expandable_variables_pattern.findall(value)
+            if variables_to_expand is not None:
+                for variable in variables_to_expand:
+                    variable_name = variable.replace('${', '').replace('}', '')
+                    variables[key] = value.replace(variable, variables[variable_name])
+
+        return variables
