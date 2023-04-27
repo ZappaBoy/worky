09b0772df485cb55c041b32f5932ca74192dbbbd
ZappaBoy <federico.zappone@justanother.cloud>
2023-04-27 15:58:36 +0200

    Upgraded version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ entry_points = \
 
 setup_kwargs = {
     'name': 'worky',
-    'version': '0.1.5',
+    'version': '0.1.6',
     'description': 'Worky is a tool that helps to define and load project workspaces.',
     'long_description': '# Worky\nManage your workspaces and increase your productivity.\n\n## What is Worky?\nWorky is a tool that helps to define and load project workspaces. It can be used to load a project workspace with a single command and quickly start your work.\nWorky saves you from wasting time doing repetitive tasks before actually starting to work.\n\n## Future improvements\nPlease note that this is a work in progress tool. If you have any idea or suggestion, please open an issue or a pull request.\nAny helps is appreciated. Here is a list of future improvements:\n- [ ] Add the project to PyPI\n\n## For developers\nInstall dependencies:\n```shell\npyenv local 3.11.1\npoetry use env 3.11.1\npoetry install\npoetry run start {{your_command_args}}\n```\n\n## Getting started\n### Installation\n### Configuration\n### Usage\n\n\n\n',
     'author': 'ZappaBoy',
