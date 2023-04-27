9f926be9897696a0caa07cde091a984803a6ce1d
ZappaBoy <federico.zappone@justanother.cloud>
2023-04-27 10:30:19 +0200

    Added command run; Added dry run option;

--- src/models/config_model.py
+++ src/models/config_model.py
@@ -1,29 +1,46 @@
 import re
 import tomllib
+from typing import List
 
 expandable_variables_pattern = re.compile(r"\$\{.*}")
+excluded_steps_entries = ['variables']
 
 
 class Config:
     variables: dict = {}
+    steps: List = []
 
     def __init__(self, config_path: str):
         self.config_path = config_path
         config_file_content = self.read_config_file(config_path)
         self.read_variables(config_file_content)
-        print(self.variables)
+        self.read_steps(config_file_content)
+        print('Variables: ', self.variables)
+        print('Steps: ', self.steps)
 
     @staticmethod
     def read_config_file(config_path: str):
         with open(config_path, 'rb') as config_file:
             return tomllib.load(config_file)
 
+    def read_steps(self, config_file_content):
+        for key, value in config_file_content.items():
+            if key not in excluded_steps_entries:
+                args = value['args'] if 'args' in value else []
+                args = [self.expand_variables(value) for value in args]
+                self.steps.append({
+                    'name': key,
+                    'command': self.variables[key],
+                    'args': args,
+                    'condition': value['condition'] if 'condition' in value else "",
+                })
+
     def read_variables(self, config_file_content):
         self.variables = config_file_content['variables']
         for key, value in self.variables.items():
             self.variables[key] = self.expand_variables(value)
 
-    def expand_variables(self, value: dict):
+    def expand_variables(self, value: str):
         variables_to_expand = expandable_variables_pattern.findall(value)
         if variables_to_expand is not None:
             for variable in variables_to_expand:
--- src/worky.py
+++ src/worky.py
@@ -1,5 +1,7 @@
 import argparse
+import subprocess
 import sys
+from typing import List
 
 from src.models.config_model import Config
 from src.utils.util import file_type
@@ -7,22 +9,58 @@ from src.utils.util import file_type
 
 class Worky:
     config: Config = None
+    flags: List[str] = []
+    dry_run: bool = False
 
     def __init__(self):
         print('Welcome to worky...')
 
-    def run(self, args=None):
+    def run(self):
+        args = self.parse_args()
+        self.load_config(args.config)
+        self.flags = args.flags
+        self.dry_run = args.dry_run
+        self.run_steps()
+
+    @staticmethod
+    def parse_args(args: List[str] = None):
         if args is None:
-            args = sys.argv
-        print('Args: ', args)
+            args = sys.argv[1:]
         parser = argparse.ArgumentParser()
         parser.add_argument('-c', '--config', type=file_type, required=False)
-        args = parser.parse_args()
-        self.load_config(args.config)
+        parser.add_argument('-f', '--flags', nargs='+', default=[], required=False)
+        parser.add_argument('--dry-run', action=argparse.BooleanOptionalAction, default=False, required=False)
+        return parser.parse_args(args)
 
     def load_config(self, config_path: str):
         print(f'Loading config from {config_path}')
         self.config = Config(config_path)
+
+    def run_steps(self):
+        dry_run_output = ''
+        for step in self.config.steps:
+            print(f'Running step {step["name"]}')
+            if step['condition'] != '' and step['condition'] not in self.flags:
+                print(f'Step {step["name"]} skipped')
+                continue
+            step_name = step['name']
+            step_command = step['command']
+            step_args = step['args']
+            print(f'Running step {step_name}: command {step_command} with args {step_args}')
+
+            args = ' '.join(step_args)
+            command = f'{step_command} {args}'
+            if self.dry_run:
+                dry_run_output += f'{command}\n'
+            else:
+                self.run_command(command)
+        if self.dry_run:
+            print(dry_run_output)
+
+    @staticmethod
+    def run_command(command: str):
+        print(f'Running command: {command}')
+        subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 
 if __name__ == '__main__':
